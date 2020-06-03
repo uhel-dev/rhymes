ddce783d2a23aa584efa8280331db56be69fe519
JakubJeziorski <jakub7575@gmail.com>
2020-06-03 10:45:41 +0100

    Update: chromedriver file for linux, Add: functionality for converting text to url.

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 import platform
+from typing import List, Any
 from selenium import webdriver
+import time
 
 
 def get_browser():
@@ -12,5 +14,19 @@ def get_browser():
         return driver
 
 
-driver = get_browser()
-driver.get('https://www.python.org')
+def get_name_of_songs():
+    driver = get_browser()
+    driver.get('https://www.hip-hop.pl/lista/?lista=1')
+    list_of_all_songs: List[Any] = []
+    list_of_all_songs.extend(driver.find_elements_by_class_name('link1'))
+    list_of_all_songs.extend(driver.find_elements_by_class_name('link2'))
+    return list_of_all_songs
+
+
+def convert_text_to_url_query():
+    list_of_all_songs = get_name_of_songs()
+    list_of_songs_text = []
+    for song in list_of_all_songs:
+        list_of_songs_text.append(song.text.replace(" ", "%20"))
+    print(list_of_songs_text)
+
